f6ff329da896c985cc9535294dac3ca4cb1b9a58
徐宁 <xuning@taobao.com>
2013-11-20 16:11:21 +0800

    test

---
+++ TestBot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestBot
+//
+//  Created by shitiven on 13-11-20.
+//  Copyright (c) 2013年 lives.im. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
